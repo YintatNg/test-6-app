0c057818b69acc24072d343bd8cb1297ab657e2e
Xianda Wu <xwu2@cougars.ccis.edu>
2019-04-05 14:25:50 -0500

    Updates

---
+++ precache-manifest.4f4df1541c1d1e7077dcd36c8b9e72c7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e7d7256f149be24da9e",
+    "url": "/test-6-app/static/js/runtime~main.089a6b75.js"
+  },
+  {
+    "revision": "ddf4bb43bfad3471b784",
+    "url": "/test-6-app/static/js/main.ff7bec0d.chunk.js"
+  },
+  {
+    "revision": "af7e1ce932894f269957",
+    "url": "/test-6-app/static/js/2.5507c1f6.chunk.js"
+  },
+  {
+    "revision": "75a869e93c3a957a62ce34c4f6a17424",
+    "url": "/test-6-app/index.html"
+  }
+];
