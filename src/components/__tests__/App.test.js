1d5a4e3350e933a31bf40b615e7d84f51af20d3d
Xianda Wu <31464273+YintatNg@users.noreply.github.com>
2019-04-23 16:35:56 -0500

    new update

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../App";
+import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 
 let wrapped;
 
 beforeEach(() => {
-    wrapped = shallow(<App />);
+  wrapped = shallow(<App />);
 });
 
-it ('shows a comment box', () => {
-    
-    expect(wrapped.find(CommentBox).length).toEqual(1);
+it("shows a comment box", () => {
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
-it ('shows a comment list', () => {
-
-    expect(wrapped.find(CommentList).length).toEqual(1);
+it("shows a comment list", () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
